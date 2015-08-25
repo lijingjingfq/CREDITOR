d44cd900e2fddeeb21741815e5dc980b5b61ac08
lijingjingfq <apple@appledeiMac.local>
2015-08-25 13:39:22 +0800

    Initial Commit

---
+++ CREDITOR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CREDITOR
+//
+//  Created by apple on 15/8/25.
+//  Copyright (c) 2015年 ZhongFuTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CREDITOR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CREDITOR
+//
+//  Created by apple on 15/8/25.
+//  Copyright (c) 2015年 ZhongFuTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
